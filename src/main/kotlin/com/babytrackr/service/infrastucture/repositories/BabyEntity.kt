bde437707e99d2ebd62e435a34c2e1a01d3dfc80
marolu20 <mariamolupitan@gmail.com>
2026-04-18 09:15:55 -0400

    add jpa repository models

---
+++ src/main/kotlin/com/babytrackr/service/infrastucture/repositories/BabyEntity.kt
@@ -0,0 +1,30 @@
+package com.babytrackr.service.infrastucture.repositories
+
+import com.babytrackr.service.controller.model.request.Sex
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
+import java.time.Instant
+
+@Entity
+@Table(name = "babies")
+class BabyEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+    var firstName: String,
+    var lastName: String,
+    var nickname: String? = null,
+    var ageMonths: Int? = null,
+    var sex: Sex? = null,
+    var userId: String? = null,
+    var createdOn: Instant,
+    var modifiedOn: Instant,
+    @OneToMany(mappedBy = "baby", fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST, CascadeType.MERGE])
+    var events: MutableList<EventEntity> = mutableListOf()
+)
