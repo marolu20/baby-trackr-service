038248d314bfdc92ecc60b8c0cd1a19ef2891a85
marolu20 <mariamolupitan@gmail.com>
2026-06-06 16:55:23 -0400

    update application.yaml

--- src/main/kotlin/com/babytrackr/service/BabyTrackrApplication.kt
+++ src/main/kotlin/com/babytrackr/service/BabyTrackrApplication.kt
@@ -1,9 +1,12 @@
 package com.babytrackr.service
 
+import com.babytrackr.service.infrastucture.config.properties.KafkaProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaProperties::class)
 class BabyTrackrApplication
 
 fun main(args: Array<String>) {
